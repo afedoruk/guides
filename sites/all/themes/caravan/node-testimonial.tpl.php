51251c455bc63446ba6e39b82f497a7e3b815430
Anna Fedoruk <afedoruk@gmail.com>
2011-01-19 01:30:04 +0300

    theme_username tweak (added picture), some other theme tweaks, user can now have only one guide card

--- sites/all/themes/caravan/node-testimonial.tpl.php
+++ sites/all/themes/caravan/node-testimonial.tpl.php
@@ -3,10 +3,12 @@ if(!$page)
 {
 	echo $content;
 	echo '<div class="alignright">'.l('Читать дальше', 'node/'.$node->nid, array("attributes"=>array("class"=>"button"))).'</div>';
+	echo '<div style="clear: left"></div>';
 }
 else
 {
 	echo "<div class='col-2 guide-preview'><ul class='img-list'>".node_view($node->guide, 1)."</ul></div>";
+	echo "<div class='submitted'>Отзыв оставил: ".theme("username", $node).", ".format_date($node->created, "custom", "d/m/Y H:i")."</div>";
 	echo $content;
 ?>
 
--- sites/all/themes/caravan/template.php
+++ sites/all/themes/caravan/template.php
@@ -110,3 +110,45 @@ function caravan_preprocess_page(&$vars)
 		}
 	}
 }
+
+function caravan_username($object) {
+
+  if ($object->uid && $object->name) {
+    // Shorten the name when it is too long or it will break many tables.
+    if (drupal_strlen($object->name) > 20) {
+      $name = drupal_substr($object->name, 0, 15) . '...';
+    }
+    else {
+      $name = $object->name;
+    }
+
+	if($object->picture)
+		$pic=theme("imagecache", "small", $object->picture);
+
+    if (user_access('access user profiles')) {
+      $output = l($pic." ".$name, 'user/' . $object->uid, array('html'=>true, 'attributes' => array('title' => t('View user profile.'))));
+    }
+    else {
+      $output = $pic." ".check_plain($name);
+    }
+  }
+  else if ($object->name) {
+    // Sometimes modules display content composed by people who are
+    // not registered members of the site (e.g. mailing list or news
+    // aggregator modules). This clause enables modules to display
+    // the true author of the content.
+    if (!empty($object->homepage)) {
+      $output = l($object->name, $object->homepage, array('attributes' => array('rel' => 'nofollow')));
+    }
+    else {
+      $output = check_plain($object->name);
+    }
+
+    $output .= ' (' . t('not verified') . ')';
+  }
+  else {
+    $output = check_plain(variable_get('anonymous', t('Anonymous')));
+  }
+
+  return $output;
+}
